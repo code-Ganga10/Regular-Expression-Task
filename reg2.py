a9a0a36af2d34c5e50edcf21790174387ebbb0f5
code-Ganga10 <ganga.sharma@gammaedge.io>
2025-02-14 16:13:58 +0530

    Updated Regx

--- reg2.py
+++ reg2.py
@@ -1,115 +1,116 @@
 import re
 
-# txt="Hello Python Module"
+txt="Hello Python Module"
 
 # # \A-Returns a match if the specified characters are at the beginning of the string
-# x=re.search("\AHello",txt)
-# print(x)
-# print()
+x=re.search("\AHello",txt)
+print(x)
+print()
 
-# x2=re.findall("\AHello",txt)
-# print(x2)
-
-
-# # \b- Returns a match where the specified characters are at the beginning or at the end of a word
-# # (the "r" in the beginning is making sure that the string is being treated as a "raw string")  
-
-# txt1="Hello Python Module "
-# x3=re.search(r"\bHello",txt1)
-# print(x3)
-# print()
-
-# x5= re.search(r"Hello\b", txt1)
-# print(x5)
-# print()
-
-# x4=re.findall(r"\bHello",txt1)
-# print(x4)
-# print()
-
-# x6= re.findall(r"Hello\b", txt1)
-# print(x6)
-# print()
+x2=re.findall("\AHello",txt)
+print(x2)
 
 
-# # ^-Starting symbol and $-Ends Symbol  and [.]= Any character expect ne line
+# \b- Returns a match where the specified characters are at the beginning or at the end of a word
+# (the "r" in the beginning is making sure that the string is being treated as a "raw string")  
 
-# txt = "hello planet menu hello"
+txt1="Hello Python Module "
+x3=re.search(r"\bHello",txt1)
+print(x3)
+print()
 
-# x = re.search("he..o", txt)
-# print(x)
+x5= re.search(r"Hello\b", txt1)
+print(x5)
+print()
 
-# x = re.findall("^he..o", txt)
-# print(x)
+x4=re.findall(r"\bHello",txt1)
+print(x4)
+print()
 
-# x = re.search("lo$", txt)
-# print(x)
-
-# x = re.findall("lo$", txt)
-# print(x)
+x6= re.findall(r"Hello\b", txt1)
+print(x6)
+print()
 
 
-# # * :Zero  or more occurence  and  + : Search one  or more  (any) characters:
-# x = re.search("he.*o", txt)
-# print(x)
-# print()
+# ^-Starting symbol and $-Ends Symbol  and [.]= Any character expect ne line
 
-# x = re.search("pl...*t", txt)
-# print(x)
-# print()
+txt = "hello planet menu hello"
 
-# x = re.search("he.+o", txt)
-# print(x)
-# print()
+x = re.search("he..o", txt)
+print(x)
 
-# x = re.findall("he.*o", txt)
-# print(x)
-# print()
+x = re.findall("^he..o", txt)
+print(x)
 
-# x = re.findall("pl...*t", txt)
-# print(x)
-# print()
+x = re.search("lo$", txt)
+print(x)
 
-# x = re.findall("he.+o", txt)
-# print(x)
-# print()
+x = re.findall("lo$", txt)
+print(x)
+
+
+# * :Zero  or more occurence  and  + : Search one  or more  (any) characters:
+x = re.search("he.*o", txt)
+print(x)
+print()
+
+x = re.search("pl...*t", txt)
+print(x)
+print()
+
+x = re.search("he.+o", txt)
+print(x)
+print()
+
+x = re.findall("he.*o", txt)
+print(x)
+print()
+
+x = re.findall("pl...*t", txt)
+print(x)
+print()
+
+x = re.findall("he.+o", txt)
+print(x)
+print()
 
 
 
 # # ? : Zero or one occurrences
-# text="Hello Python Regular Hello"
-# x=re.search("He..?o",text)
-# print(x)
-# print()
+text="Hello Python Regular Hello"
+x=re.search("He..?o",text)
+print(x)
+print()
 
-# text="Hello Python Regular"
-# x=re.findall("He..?o",text)
-# print(x)
-# print()
+text="Hello Python Regular"
+x=re.findall("He..?o",text)
+print(x)
+print()
 
 
 txt = "hello planet"
 
 # {} : Exactly the specified number of occurrences
-# x = re.findall("he.{2}o", txt)
-# print(x)
-# print()
+x = re.findall("he.{2}o", txt)
+print(x)
+print()
 
-# x1=re.findall("pl.{3}t",txt)
-# print(x1)
-# print()
+x1=re.findall("pl.{3}t",txt)
+print(x1)
+print()
 
-# x1=re.search("pl.{3}t",txt)
-# print(x1)
-# print()
+x1=re.search("pl.{3}t",txt)
+print(x1)
+print()
 
 # | Either or 	"falls|stays" :- Check if the string contains either "falls" or "stays":	
 
 text2="THe rain falls stays "
+x2=re.search("falls|stays",text2)
+print(x2)
 
 text3="The python"
-# x2=re.search("falls|stays",text2)
-# print(x2)
+
 x3=re.search("falls|stays",text3)
 print(x3)
 print()
